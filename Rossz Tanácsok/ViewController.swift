64329e2f1f554c615eefdd286aa9c5c3e7377290
Péter Bálint <pbalint999@gmail.com>
2018-08-24 10:26:15 +0200

    Initial Commit

---
+++ Rossz Tanácsok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rossz Tanácsok
+//
+//  Created by Péter Bálint on 2018. 08. 24..
+//  Copyright © 2018. Péter Bálint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
